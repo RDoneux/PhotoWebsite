b02fc755d3c82cf4b79a8213aa59bff98b903cd9
rdoneux <robertdoneux@gmail.com>
2022-12-12 14:43:07 +0000

    sets up frontend for message saving

--- frontend/src/app/pages/contact/contact.component.ts
+++ frontend/src/app/pages/contact/contact.component.ts
@@ -3,8 +3,26 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  styleUrls: ['./contact.component.scss'],
 })
 export class ContactComponent {
+  name: string | undefined = undefined;
+  subject: string | undefined = undefined;
+  message: string | undefined = undefined;
 
+  onNameChange(event: string) {
+    this.name = event;
+  }
+
+  onSubjectChange(event: string) {
+    this.subject = event;
+  }
+
+  onMessageChange(event: string) {
+    this.message = event;
+  }
+
+  onSubmit() {
+    console.log(this.name, ' : ', this.subject, ' : ', this.message);
+  }
 }
