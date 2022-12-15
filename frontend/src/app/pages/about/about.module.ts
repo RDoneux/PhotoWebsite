0a58273aefba77691f6c69ecd5bd553744462afa
rdoneux <robertdoneux@gmail.com>
2022-12-15 14:20:29 +0000

    adds about page

--- frontend/src/app/pages/about/about.module.ts
+++ frontend/src/app/pages/about/about.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { AboutRoutingModule } from './about-routing.module';
 import { AboutComponent } from './about.component';
 import { NavBarModule } from 'src/app/user-interface/nav-bar/nav-bar.module';
+import { PageTitleModule } from 'src/app/common/page-title/page-title.module';
 
 @NgModule({
   declarations: [AboutComponent],
-  imports: [CommonModule, AboutRoutingModule, NavBarModule],
+  imports: [CommonModule, AboutRoutingModule, NavBarModule, PageTitleModule],
 })
 export class AboutModule {}
